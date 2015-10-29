406b5541b8a835db494f2bfd79868b989a0b55cb
Pau Fernández <pau.fernandez@upc.edu>
2015-10-29 11:42:14 +0100

    Funciona la detección del tiempo de screen on/off

--- app/src/main/java/info/pauek/dontwork/DontWorkService.java
+++ app/src/main/java/info/pauek/dontwork/DontWorkService.java
@@ -2,7 +2,10 @@ package info.pauek.dontwork;
 
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -12,14 +15,22 @@ import android.util.Log;
  */
 public class DontWorkService extends Service {
 
+    ScreenOnOffReceiver receiver;
+
+
+
     @Override
     public void onCreate() {
-        Log.i("DontWork", "Created!");
+        receiver = new ScreenOnOffReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(receiver, filter);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("LocalService", "Received start id " + startId + ": " + intent);
+        Log.i("DontWork", "Received start id " + startId + ": " + intent);
         return START_STICKY;
     }
 
@@ -27,5 +38,20 @@ public class DontWorkService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public class ScreenOnOffReceiver extends BroadcastReceiver {
+
+        private long lastOn;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                long diff = System.currentTimeMillis() - lastOn;
+                Log.i("DontWork", String.format("Screen on: %.3f seconds", (float)diff / 1000.0f));
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                lastOn = System.currentTimeMillis();
+            }
+        }
     }
 }
